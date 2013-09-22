0592455ca248488c34eb629cf45a9e793a26135b
Prithvi Prabahar <prithvi923@gmail.com>
2013-09-21 20:24:18 -0400

    Add MongoDB connection info

--- quakes.rb
+++ quakes.rb
@@ -1,4 +1,10 @@
-require 'sinatra'
+Bundler.require
+
+# TODO: Create a YAML config file
+configure do
+  MongoMapper.setup({'production' => {'uri' => ENV['MONGOLAB_URI']}}, 'production')
+  MongoMapper.setup({'development' => {'uri' => 'mongodb://localhost/quaker'}}, 'development')
+end
 
 get '/' do
   "Hello World!"
