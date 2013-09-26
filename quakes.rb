3bb18b372f34cdfd0c0d6a4c049f239ab9772e7f
Prithvi Prabahar <prithvi923@gmail.com>
2013-09-26 01:22:31 -0400

    Start storing data in MongoDB and setup scheduler

--- quakes.rb
+++ quakes.rb
@@ -9,6 +9,40 @@ configure do
   end
 end
 
+# TODO: Create separate model file
+class Place
+  include MongoMapper::Document
+
+  key :mag,   Float
+  key :place, String
+  key :time, Time
+  key :location, Array
+
+  ensure_index [[:location, '2dsphere']]
+  ensure_index [[:location, 1], [:time, -1]], :unique => true
+
+end
+
+# TODO: Place in separate file potentially
+scheduler = Rufus::Scheduler.start_new
+scheduler.every '15m' do
+  update_db
+end
+
+def update_db
+  data = RestClient.get "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
+  data = JSON.parse data
+  features = data["features"]
+  features.each do |feature|
+    Place.create({
+        :mag => feature['properties']['mag'],
+        :place => feature['properties']['place'],
+        :time => Time.at(feature['properties']['time']/1000),
+        :location => [feature['geometry']['coordinates'][0], feature['geometry']['coordinates'][1]]
+      })
+  end
+end
+
 get '/' do
-  "Hello World!"
+  "Hello, world!"
 end
