2b7a0c94e0e2abe169e479f2ad73be47a79a2821
Prithvi Prabahar <prithvi923@gmail.com>
2013-09-26 01:22:31 -0400

    Make it work
    Start storing data in MongoDB and setup scheduler
    
    Add querying for count and days
    
    Use mySQL instead of MongoDB
    
    Create separate model file for Place
    
    Configure regions
    
    Include model in quakes.rb to work with Heroku
    
    Clean up quakes
    
    Separate code from each other
    
    Try to fix Heroku issue

--- quakes.rb
+++ quakes.rb
@@ -1,14 +1,38 @@
 Bundler.require
-
-# TODO: Create a YAML config file
-configure do
-  if ENV['MONGOLAB_URI'] then
-    MongoMapper.setup({'production' => {'uri' => ENV['MONGOLAB_URI']}}, 'production')
-  else
-    MongoMapper.setup({'development' => {'uri' => 'mongodb://localhost/quaker'}}, 'development')
-  end
-end
+require_relative 'models/init'
+require_relative 'helpers/init'
 
 get '/' do
-  "Hello World!"
+  "Hello, world!"
+end
+
+get '/quakes' do
+  content_type :json
+
+  count = 10
+  days = 10
+
+  if params[:count] then
+    count = params[:count].to_i
+  end
+  if params[:days] then
+    days = params[:days].to_i
+  end
+  days_ago = Time.now() - 3600*24*days
+  if params[:region] then
+    places = repository(:default).adapter.select(
+      "SELECT p2place AS place, time, p2lat AS lat, p2lon AS lon, p2mag AS mag, AVG(p1mag) AS avg_mag
+      FROM (SELECT p1.mag AS p1mag, p1.place AS p1place, p1.lat AS p1lat, p1.lon AS p1lon, p2.mag AS p2mag, p2.place AS p2place, p2.lat AS p2lat, p2.lon AS p2lon, p2.time, (3959 * acos( cos( radians(p1.lat) ) * cos( radians( p2.lat ) ) * cos( radians( p2.lon ) - radians(p1.lon) ) + sin( radians(p1.lat) ) * sin( radians( p2.lat ) ) ) ) AS distance
+      FROM places AS p1, places AS p2
+      HAVING distance < 25) AS region
+      WHERE UNIX_TIMESTAMP(time) >= #{days_ago.to_i}
+      GROUP BY p2place
+      ORDER BY avg_mag DESC
+      LIMIT 0, #{count};"
+    )
+    places.map! { |place| Hash[place.members.zip(place.values)]}
+    places.to_json
+  else
+    Place.all(:time.gte => days_ago, :order => [:mag.desc], :limit => count).to_json
+  end
 end
