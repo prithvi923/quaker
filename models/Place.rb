d0f7c449c2e740188c0ed041c87adfa75c798275
Prithvi Prabahar <prithvi923@gmail.com>
2013-10-02 12:27:16 -0400

    Move SQL logic from controller into model

--- models/Place.rb
+++ models/Place.rb
@@ -12,4 +12,38 @@ class Place
   property :time,    Time, :key => true
   property :lat,     Float, :key => true
   property :lon,     Float, :key => true
+
+  def self.find_results(count, days, wantsRegion)
+    # Convert time to Unix Timestamp
+    days = Time.now() - 3600*24*days
+    # If region is true, then use custom SQL query that returns #{count} places that all experienced earthquakes within #{days} ago, using magnitudes within 25 miles that also occurred #{days} ago
+    if wantsRegion then
+      places = repository(:default).adapter.select(
+        "SELECT p2place AS place, time, p2lat AS lat, p2lon AS lon, p2mag AS mag, AVG(p1mag) AS avg_mag
+        FROM (SELECT p1.mag AS p1mag, p1.place AS p1place, p1.lat AS p1lat, p1.lon AS p1lon, p2.mag AS p2mag, p2.place AS p2place, p2.lat AS p2lat, p2.lon AS p2lon, p2.time, (3959 * acos( cos( radians(p1.lat) ) * cos( radians( p2.lat ) ) * cos( radians( p2.lon ) - radians(p1.lon) ) + sin( radians(p1.lat) ) * sin( radians( p2.lat ) ) ) ) AS distance
+        FROM places AS p1, places AS p2
+        HAVING distance < 25) AS region
+        WHERE UNIX_TIMESTAMP(time) >= #{days.to_i}
+        GROUP BY p2place
+        ORDER BY avg_mag DESC
+        LIMIT 0, #{count};"
+      )
+      # Transform structs into hashes
+      places.map! { |place| Hash[place.members.zip(place.values)]}
+      # Remove avg_mag from hashes
+      places.each { |place| place.delete(:avg_mag) }
+    else
+      # Otherwise, use DataMapper to query the Places
+      Place.all(:time.gte => days, :order => [:mag.desc], :limit => count) 
+    end
+  end
+
+  private
+
+    # Helper function to convert between string and boolean
+    def to_boolean(str)
+      return true if str.downcase == "true"
+      return false if str.downcase == "false"
+      raise ArgumentError
+    end
 end
--- routes/quakes.rb
+++ routes/quakes.rb
@@ -21,7 +21,7 @@ class MyQuaker < Sinatra::Application
     end
 
     # Otherwise, query MySQL for payload
-    payload = get_payload(count, days, wantsRegion)
+    payload = Place.find_results(count, days, wantsRegion)
 
     # Store in cache
     $redis.set(hash, payload.to_json)
@@ -59,37 +59,5 @@ class MyQuaker < Sinatra::Application
       @badParams = true
       false
     end
-  end
-
-  def get_payload(count, days, wantsRegion)
-    # Convert time to Unix Timestamp
-    days = Time.now() - 3600*24*days
-    # If region is true, then use custom SQL query that returns #{count} places that all experienced earthquakes within #{days} ago, using magnitudes within 25 miles that also occurred #{days} ago
-    if wantsRegion then
-      places = repository(:default).adapter.select(
-        "SELECT p2place AS place, time, p2lat AS lat, p2lon AS lon, p2mag AS mag, AVG(p1mag) AS avg_mag
-        FROM (SELECT p1.mag AS p1mag, p1.place AS p1place, p1.lat AS p1lat, p1.lon AS p1lon, p2.mag AS p2mag, p2.place AS p2place, p2.lat AS p2lat, p2.lon AS p2lon, p2.time, (3959 * acos( cos( radians(p1.lat) ) * cos( radians( p2.lat ) ) * cos( radians( p2.lon ) - radians(p1.lon) ) + sin( radians(p1.lat) ) * sin( radians( p2.lat ) ) ) ) AS distance
-        FROM places AS p1, places AS p2
-        HAVING distance < 25) AS region
-        WHERE UNIX_TIMESTAMP(time) >= #{days.to_i}
-        GROUP BY p2place
-        ORDER BY avg_mag DESC
-        LIMIT 0, #{count};"
-      )
-      # Transform structs into hashes
-      places.map! { |place| Hash[place.members.zip(place.values)]}
-      # Remove avg_mag from hashes
-      places.each { |place| place.delete(:avg_mag) }
-    else
-      # Otherwise, use DataMapper to query the Places
-      Place.all(:time.gte => days, :order => [:mag.desc], :limit => count) 
-    end
-  end
-
-  # Helper function to convert between string and boolean
-  def to_boolean(str)
-    return true if str.downcase == "true"
-    return false if str.downcase == "false"
-    raise ArgumentError
   end
 end
